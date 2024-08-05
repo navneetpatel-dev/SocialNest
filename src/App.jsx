eddcb40e91ab3f8c7cc196a9e4034878ba7dd012
Navneet Patel <navneetp22875@gmail.com>
2024-08-05 20:56:40 +0530

    chore: Refactor app structure and update dependencies

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Social Nest</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
